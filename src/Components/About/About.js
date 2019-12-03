6ea4dfcf22582d674796533fe07da61021bcacb5
Roey Shmool <roey.shmool@gmail.com>
2019-12-02 21:02:54 -0600

    add components

---
+++ src/Components/About/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class About extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h2>About</h2>
+                <p>We act like we react to the acting of reaction.</p>
+            </div>
+        )
+
+    }
+}
+
+export default About;
---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Contact extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h2>Contact</h2>
+                <p>Phone: 1-800-I-REACTS</p>
+                <p>Email: info@ReactActors.com</p>
+            </div>
+        )
+
+    }
+}
+
+export default Contact;
